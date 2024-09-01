04050c8f1f527b101075ebd6cc271b2c52a84803
Ye Minn Thein <yeminnthein.sg@gmail.com>
2024-09-01 23:26:36 +0800

    Add just comments

---
+++ script.js
@@ -0,0 +1,9 @@
+// make function named 'getComputerChoice'
+
+// make function named 'getHumanChoice'
+
+// make new variable 'humanScore' and 'computerScore'
+
+// make new function named 'playRound'
+
+// make new function named 'playGame'
