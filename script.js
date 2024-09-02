bbdc4cd71c21137078ef4ae39b3b4259057e21a4
Ye Minn Thein <yeminnthein.sg@gmail.com>
2024-09-02 10:39:53 +0800

    Write down psudocodes

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 // make function named 'getComputerChoice'
+/*
+    Steps:
+    Computer play random (Rock / Paper / Scissors)
+        v
+    Need random number between (0 - 3)
+        v
+    Change number to text (e.g. 1 means Rock)
+        v
+    Show text as choice
+*/
 
 // make function named 'getHumanChoice'
+/*
+    Steps:
+    You need to choose with text
+        v
+    Need to compare with computer's choice
+        v
+    Which side will win?
+        v
+    Show the winner for this round and add point
+        v
+    Next round
+*/
 
 // make new variable 'humanScore' and 'computerScore'
+/*
+    Steps:
+    If you get 5 points, you win
+        or
+    If computer get 5 points, computer win
+*/
 
 // make new function named 'playRound'
 
